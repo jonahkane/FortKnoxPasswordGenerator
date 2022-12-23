d619247e7e71b8921cd4a1dc23e89bed2bb8ebc7
Jonah <jonahtkane@gmail.com>
2022-12-22 20:27:45 -0600

    intial commit of full code package

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# FortKnoxPasswordGenerator
+# Random Password Generator AKA FortKnoxPasswordGenerator
+
+## Description
+
+This application can be used to generate a random password.
+
+## Installation
+
+N/A
+
+## Usage
+
+To use this application, the user will click the generate password button. Upon clicking this the user will be presented with a series of prompts asking for input. The first input will ask the user to select the length of the password between 8 & 128 characters long. They will then be asked if they want to include characters from the following subcategories : symbols, numbers, lowercase letters and finally uppercase letters. After responding to the promts the password generator will quickly and effortlessly generate and display a highly secure randomized password.
+
+## Credits
+
+N/A
+
+## License
+
+Please refer to the LICENSE in the repo if applicable.
---
+++ script.js
@@ -0,0 +1,70 @@
+// Assignment code here
+
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+function writePassword() {
+
+const symbolsString =  " !#$%&'()*+,-./:;<=>?@[]^_`{|}~"
+const digitsString = "1234567890";
+const lowerString = "abcdefghijklmnopqrstuvwxyz";
+const upperString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+let symbolsArray = symbolsString.split("");
+let digitsArray = digitsString.split("");
+let lowerArray = lowerString.split("");
+let upperArray = upperString.split("");
+
+
+
+let passwordArray = [];
+let confirmLength = prompt("how many characters would you like your password to contain? Please enter a number from 8-128.")
+let confirmSymbols = confirm("Click OK if you want to include symbols, otherwise click cancel");
+let confirmDigits = confirm("Click OK if you want to include numbers/digits, otherwise click cancel")
+let confirmLower = confirm("Click OK if you want to include lowercase letters , otherwise click cancel");
+let confirmUpper = confirm("Click OK if you want to include uppercase letters, otherwise click cancel");
+
+// if (confirmLength <8 || confirmLength>128){alert("please enter a value from 8-128")};
+
+if (confirmSymbols === true){
+passwordArray = passwordArray.concat(symbolsArray)};
+if (confirmDigits === true){
+passwordArray = passwordArray.concat(digitsArray)};
+if (confirmLower === true){
+passwordArray = passwordArray.concat(lowerArray)};
+if (confirmUpper === true){
+passwordArray = passwordArray.concat(upperArray)};
+  
+// console.log(passwordArray);
+console.log("password length is : " + confirmLength);
+
+
+// use math.random to generate password string (taken from rock paper scissors game... may need to retool this code)
+let passwordLength = confirmLength;
+
+//create function to randomize password 
+function randomPassword (){
+  let generatedPassword = [];
+  for (let i = 0; i < passwordLength; i++) {
+    let randomNumber = Math.floor(Math.random()*passwordArray.length)
+    generatedPassword += passwordArray[randomNumber]
+  }
+  return generatedPassword;
+}
+randomPassword();
+let password = randomPassword();
+return password;
+}
+
+//this console.log is returning the text from the HTML document, NOT the password array
+let finalPassword = writePassword();
+console.log(finalPassword);
+  let passwordText = document.querySelector("#password");
+    
+  passwordText.value = finalPassword;
+  
+
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
